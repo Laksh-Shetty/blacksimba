14fd122ebad3191104a8e825d15205e5daba9f09
JOD <bluecr07@gmail.com>
2025-03-11 10:26:00 +0530

    js

--- animation.js
+++ animation.js
@@ -54,3 +54,50 @@ function init() {
     window.addEventListener('resize', onWindowResize);
     animate();
 }
+function checkPerformance() {
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    const hasLowPixelRatio = window.devicePixelRatio < 2;
+    isLowPerformance = isMobile || hasLowPixelRatio;
+    if (isLowPerformance) {
+        console.log('Low performance mode enabled');
+    }
+}
+
+function addLights() {
+    const ambientLight = new THREE.AmbientLight(0x404040, 0.5);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+    const orangeLight = new THREE.PointLight(0xe65b07, 3, 20);
+    orangeLight.position.set(-5, 2, -3);
+    scene.add(orangeLight);
+    const secondaryLight = new THREE.PointLight(0xb34605, 3, 20);
+    secondaryLight.position.set(5, 0, -2);
+    scene.add(secondaryLight);
+    const spotlight = new THREE.SpotLight(0xffffff, 2, 30, Math.PI / 6, 0.5, 1);
+    spotlight.position.set(0, 10, 5);
+    spotlight.castShadow = true;
+    spotlight.shadow.mapSize.width = 1024;
+    spotlight.shadow.mapSize.height = 1024;
+    scene.add(spotlight);
+}
+
+function setupPostProcessing() {
+    composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+    const bloomPass = new UnrealBloomPass(
+        new THREE.Vector2(window.innerWidth, window.innerHeight),
+        0.7,
+        0.3,
+        0.7
+    );
+    composer.addPass(bloomPass);
+    const fxaaPass = new ShaderPass(FXAAShader);
+    fxaaPass.material.uniforms['resolution'].value.set(
+        1 / (window.innerWidth * renderer.getPixelRatio()),
+        1 / (window.innerHeight * renderer.getPixelRatio())
+    );
+    composer.addPass(fxaaPass);
+}
